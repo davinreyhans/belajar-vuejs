0b6cd55637f5e4a8a305d28f9c3f1ef08f516dad
davinreyhans <[daavinreyhans@gmail.com]>
2023-07-02 22:39:05 +0700

    first commit

---
+++ index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+        <input v-model="inputText" type="text">
+        <button @click="changeMessage">Ubah Pesan</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Halo, dunia!',
+            inputText: ''
+        };
+    },
+    methods: {
+        changeMessage() {
+            this.message = this.inputText;
+            this.inputText = '';
+        }
+    }
+};
+</script>
+
+<style>
+h1 {
+    color: blue;
+}
+</style>
